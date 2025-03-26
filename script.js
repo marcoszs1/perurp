904cf6345fae2fdaecfe9ad170ecee127d1fef5a
marcoszs1 <marquitosruizv90@gmail.com>
2025-03-26 00:39:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Página cargada correctamente.");
-});
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
